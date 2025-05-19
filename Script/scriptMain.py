6769a620079b39fc4edcbe9be9f983d444f90e0b
Iair Feigelman <feigelmaniair@gmail.com>
2025-05-19 17:55:22 -0300

    Agregando log de conversaciones

--- Script/scriptMain.py
+++ Script/scriptMain.py
@@ -1,12 +1,13 @@
 import random
 import os
 import difflib
+import datetime
 
 # Librerías originales
 articulos = ["el", "la", "los", "las", "un", "una", "unos", "unas", "al", "del", "es", "de", "que", "en",
  "quien", "por", "para", "con", "a", "y", "o", "si", "no", "como", "mas", "menos", "muy", "todo", "toda", "todos", "todas",'cual','fue','quienes']
 
-personajesYsalida = ["cambiar", "personaje", "adios", "salir", 'r2d2', 'c-3po', 'yoda', 'chewbacca','c3po']
+palabrasClaves = ["cambiar", "personaje", "adios", "salir", 'r2d2', 'c-3po', 'yoda', 'chewbacca','c3po']
 
 vocalesTildes = ["á", "é", "í", "ó", "ú"]
 vocalesSinTilde = ['a', 'e', 'i', 'o', 'u']
@@ -21,9 +22,20 @@ existeArchivoPreguntas = True
 existeCarpetaPreguntas = True
 existeCarpetaLogs = True
 entradaOriginal = ''
+entradaModificada = ''
+huboCorreccionOrtografica = False
+
 
 
 def crearArchivoPreguntas():
+    """
+    Crea el archivo 'ArchivosDeLectura/preguntas.txt' con un conjunto de preguntas y respuestas,
+    incluyendo variantes de preguntas y respuestas en estilo convencional y en estilo Yoda.
+    Si ocurre un error durante la creación o escritura del archivo, se maneja la
+    excepción llamando a la función 'manejarError'.
+
+    Excepciones: Si ocurre un error al crear o escribir en el archivo, la excepción es capturada y gestionada.
+    """
     try:
         with open("ArchivosDeLectura/preguntas.txt", "w", encoding="utf-8") as file:
         # Preguntas iniciales sobre Star Wars
@@ -98,6 +110,20 @@ def crearArchivoPreguntas():
         return
 
 def verificarArchivos():
+    """
+    Verifica la existencia de las carpetas y archivos necesarios para el funcionamiento del programa.
+    Si no existen, los crea e inicializa con contenido predeterminado.
+    Acciones realizadas:
+    - Verifica y crea la carpeta 'ArchivosDeLectura' si no existe.
+    - Verifica y crea el archivo 'preguntas.txt' dentro de 'ArchivosDeLectura' si no existe, utilizando la función crearArchivoPreguntas().
+    - Verifica y crea la carpeta 'Logs' si no existe.
+    - Verifica y crea el archivo 'errorLogs.txt' dentro de 'Logs' si no existe, inicializándolo con un encabezado.
+    - Maneja y reporta errores durante la creación de carpetas y archivos.
+
+    Excepciones:
+    - Captura y maneja excepciones durante la creación de carpetas y archivos.
+    Excepciones: Llama a la función manejarError en caso de error general.
+    """
     try:
         # Verificar si la carpeta existe, si no existe, la crea
         if not os.path.exists("ArchivosDeLectura"):
@@ -110,6 +136,7 @@ def verificarArchivos():
             except Exception as e:
                 # Manejar el error si no se puede crear la carpeta
                 print(f"Error al crear la carpeta: {e}")
+                verificarArchivos()
                 return
             print("Carpeta 'ArchivosDeLectura' creada.")
             
@@ -120,6 +147,7 @@ def verificarArchivos():
             except Exception as e:
                 # Manejar el error si no se puede crear el archivo
                 print(f"Error al crear el archivo: {e}")
+                verificarArchivos()
                 return
             print("Archivo 'preguntas.txt' creado con preguntas iniciales.")
 
@@ -151,25 +179,47 @@ def verificarArchivos():
             except Exception as e:
                 # Manejar el error si no se puede crear el archivo
                 print(f"Error al crear el archivo: {e}")
+                verificarArchivos()
                 return
             print("Archivo 'errorLogs.txt' creado.")
+        try:
             if not os.path.exists("Logs/errorLogs.txt"):
                 # Crear el archivo errorLogs.txt dentro de la carpeta Logs
                 print("Creando archivo errorLogs.txt...")
                 with open("Logs/errorLogs.txt", "w", encoding="utf-8") as file:
                     file.write("Registro de errores:\n")
                 print("Archivo 'errorLogs.txt' creado.")
+        except Exception as e:
+            # Manejar el error si no se puede crear el archivo
+            print(f"Error al crear el archivo: {e}")
+            verificarArchivos()
+            return
+        if not os.path.exists("Logs/log.txt"):
+            # Crear el archivo log.txt par guardar las interacciones
+            print("Creando archivo log.txt...")
+            with open("Logs/log.txt", "w", encoding="utf-8") as file:
+                file.write("Registro de log:\n")
+            print("Archivo 'log.txt' creado.")                
     except Exception as e:
         manejarError(e, "Error al verificar o crear archivos")
 
-
 def inicioPrograma():
+    """
+    Inicia el programa principal del chatbot, permitiendo al usuario seleccionar un personaje para conversar.
+    Verifica la existencia de archivos necesarios, muestra instrucciones, solicita el nombre del personaje,
+    corrige la ortografía del nombre ingresado y gestiona la elección del personaje.
+    Maneja interrupciones por teclado y otros errores inesperados.
+
+    Excepciones:
+    KeyboardInterrupt: Si el usuario interrumpe la ejecución con Ctrl+C.
+    Exception: Para cualquier otro error inesperado, delegando el manejo a la función manejarError.
+    """
     try:
         verificarArchivos()
         print("Podés chatear con distintos personajes como R2D2, Chewbacca, Yoda o C-3PO. Cuando desees cambiar de personaje, escribí: cambiar personaje.\n")
         print("En caso que desee salir del programa escriba: salir o adios.")
         personaje = input("Coloque el nombre del personaje con el que desea hablar: ")
-        personaje = ortografia(personaje,personajesYsalida)
+        personaje = ortografia(personaje,palabrasClaves)
         personaje = ' '.join(personaje)
         eleccionPersonaje(personaje)
     except KeyboardInterrupt:
@@ -177,8 +227,61 @@ def inicioPrograma():
     except Exception as e:
         manejarError(e, "Error inesperado en el inicio del programa")
 
+def agregarInteraccionLogs(entradaOriginal, entradaCorregida, respuesta, personaje):
+    """
+    Agrega la interacción del usuario y la respuesta del personaje al archivo de log.
+    Si el archivo no existe, lo crea y agrega un encabezado.
+    Maneja excepciones durante la escritura en el archivo de log.
+
+    Excepciones:
+        FileNotFoundError: Si el archivo de log no existe, lo crea.
+        Exception: Maneja cualquier otra excepción durante la escritura en el log.
+    """
+    try:
+        with open("Logs/log.txt", "a", encoding="utf-8") as file:
+            file.write("Fecha y hora: " + str(datetime.datetime.now()) + "\n")
+            file.write(f"Pregunta con correccion ortografica: {entradaCorregida}\n")
+            if huboCorreccionOrtografica == True:
+                file.write(f"Pregunta sin correcion ortografica: {entradaOriginal}\n")
+            file.write(f"Respuesta: {respuesta}\n")
+            file.write(f"Personaje: {personaje}\n")
+            if agregoPregunta == True:
+                file.write("Porcentaje de acierto: 0%")
+            else:
+                file.write("Porcentaje de acierto: 100%")         
+            file.write("\n")
+            file.write("----------------------------------------\n")
+    except FileNotFoundError:
+        with open("Logs/log.txt", "w", encoding="utf-8") as file:
+            verificarArchivos()
+            with open("Logs/errorLogs.txt", "a", encoding="utf-8") as file:
+                file.write("Error: No se encontró el archivo de log. Se ha creado uno nuevo.\n")
+                agregarInteraccionLogs(entradaOriginal, entradaCorregida, respuesta, personaje)
+    except Exception as e:
+        manejarError(e, "Error al agregar interacción al log")
+    
+
 
 def agregarPregunta():
+    """
+    Agrega una nueva pregunta y su respuesta al archivo de preguntas después de la confirmación del usuario.
+    Solicita al usuario confirmar si desea agregar la pregunta actual (`entradaOriginal`) al sistema.
+    Si confirma, pide la respuesta, agrega ambas al archivo y actualiza las listas correspondientes.
+    Maneja la ausencia del archivo o carpeta intentando crearlos y reintentando la operación.
+    Captura y maneja otras excepciones usando el manejador de errores personalizado.
+    
+    Variables globales:
+        entradaOriginal (str): La pregunta a agregar.
+        agregoPregunta (bool): Indicador de si se agregó una pregunta.
+        newAnsw (list): Lista para almacenar nuevas respuestas.
+        newQuest (list): Lista para almacenar nuevas preguntas.
+        existeArchivoPreguntas (bool): Indica si existe el archivo de preguntas.
+        existeCarpetaPreguntas (bool): Indica si existe la carpeta de preguntas.
+        
+    Excepciones:
+        FileNotFoundError: Si el archivo o carpeta de preguntas no existe, intenta crearlos y reintenta.
+        Exception: Maneja cualquier otra excepción usando `manejarError`.
+    """
     try:
         global entradaOriginal
         with open("ArchivosDeLectura/preguntas.txt", "a", encoding="utf-8") as file:
@@ -225,7 +328,6 @@ def LstPalabrasClaves():
                             palabrasClaves.append(palabra)
 
         palabrasClaves = list(set(palabrasClaves))
-        palabrasClaves.extend(personajesYsalida)
         return palabrasClaves
     except FileNotFoundError:
         verificarArchivos()
@@ -240,11 +342,28 @@ def LstPalabrasClaves():
 
 
 def ortografia(entrada, listado):
+        """
+        Corrige errores ortográficos en una entrada de texto comparando cada palabra con un listado de palabras válidas.
+        Parámetros:
+            entrada (str): La cadena de texto a corregir.
+            listado (list): Lista de palabras válidas para comparar y corregir.
+        Retorna:
+            list: Lista de palabras corregidas según el listado proporcionado. Si ocurre un error, retorna la entrada original.
+        Notas:
+            - Utiliza coincidencias aproximadas para sugerir correcciones ortográficas.
+            - Solicita confirmación al usuario antes de realizar una corrección.
+            - Omite artículos definidos en la variable global 'articulos'.
+        Exceociones:
+            En caso de error, maneja la excepción y retorna la entrada original.
+        """
         global entradaOriginal
+        global entradaModificada
+        global huboCorreccionOrtografica
+        huboCorreccionOrtografica = False
         try:
             lista_palabras = entrada.split()
         
-        
+            entradaOriginal = entrada
             entrada = limpiadorFrases(entrada)
             salida = []
         
@@ -256,13 +375,13 @@ def ortografia(entrada, listado):
                     salida.append(palabra)
                     continue
                 try:
-                    coincidencias = difflib.get_close_matches(palabra, listado, n=2, cutoff=0.5)
+                    coincidencias = difflib.get_close_matches(palabra, listado, n=3, cutoff=0.5)
                 except Exception as e:
                     manejarError(e, "Error en la búsqueda de coincidencias")
                     continue
                 i = 0
                 corregida = False
-                while i < len(coincidencias) and  i < 2 :
+                while i < len(coincidencias) and  i < 3 :
                     if palabra == coincidencias[i]:
                         break
                     print(f"Palabra: {palabra} - ¿Quisiste decir '{coincidencias[i]}'?")
@@ -275,18 +394,18 @@ def ortografia(entrada, listado):
                     if respuesta == 'si':
                         salida.append(coincidencias[i])
                         lista_palabras[posicion] = coincidencias[i]
-
+                        huboCorreccionOrtografica = True
                         corregida = True
                         break
 
                     i += 1
 
                 if not corregida:
-                    entradaOriginal = []
+                    entradaModificada = []
 
                     salida.append(palabra)
                     
-            entradaOriginal = ' '.join(lista_palabras)
+            entradaModificada = ' '.join(lista_palabras) 
             return salida
         except Exception as e:
             manejarError(e, "Error en la corrección ortográfica")
@@ -295,6 +414,19 @@ def ortografia(entrada, listado):
 
 
 def buscarRespuesta(userInput, questGroup, answGroup):
+    """
+    Busca la mejor respuesta para una entrada de usuario comparando con grupos de preguntas y respuestas.
+    Prarametros:
+        userInput (str): Entrada del usuario a analizar.
+        questGroup (list[list[str]]): Lista de grupos de preguntas, donde cada grupo es una lista de frases relacionadas.
+        answGroup (list[list[str]]): Lista de grupos de respuestas, donde cada grupo es una lista de respuestas correspondientes a questGroup.
+    Returns:
+        str: La mejor respuesta encontrada según la similitud con la entrada del usuario, o un mensaje predeterminado si no se encuentra coincidencia suficiente.
+    Notas:
+        - Utiliza coincidencia de palabras y densidad de coincidencias para determinar la mejor respuesta.
+    Excepciones:
+        Si ocurre una excepción, maneja el error y retorna un mensaje predeterminado.
+    """
     mejorIndice = -1
     mejorPuntaje = -1
     umbral = 0.8
@@ -327,6 +459,17 @@ def buscarRespuesta(userInput, questGroup, answGroup):
 
 
 def eleccionPersonaje(personaje):
+    """
+    Permite al usuario elegir un personaje de Star Wars predefinidos para interactuar en una conversación simulada.
+    El usuario puede seleccionar entre los personajes 'yoda', 'chewbacca', 'r2d2', 'c-3po'. 
+    Durante la conversación, el usuario puede cambiar de personaje, finalizar la conversación o hacer preguntas al personaje seleccionado.
+    Dependiendo del personaje, las respuestas pueden ser frases predefinidas o respuestas generadas a partir de la entrada del usuario.
+    Si el sistema no tiene respuesta para una pregunta, se le solicita al usuario agregar la pregunta al sistema.
+    Parámetros:
+        personaje (str): El nombre del personaje con el que el usuario desea interactuar inicialmente.
+    Excepciones:
+        Maneja errores relacionados con la elección del personaje y el procesamiento de preguntas, mostrando mensajes apropiados al usuario.
+    """
     # Se define la función eleccionPersonaje que permite al usuario elegir un personaje para interactuar
     try:
         palabrasClaves = LstPalabrasClaves()
@@ -381,17 +524,11 @@ def eleccionPersonaje(personaje):
                     primeraVez = False
                     try:
                         entrada = entrada.lower().strip("¿?#$%&/()!¡-_[]}{.,;:<>")
-                        print(entrada)
-
                         entrada = ortografia(entrada,palabrasClaves)
-                        if entrada[0] in ["salir", "adios"]:
-                                print("Conversación finalizada, que la fuerza te acompañe.")
-                                break
                         if personaje == 'yoda':
                             respuesta = lectorPregunta(entrada, True)
                         else:
                             respuesta = lectorPregunta(entrada, False)
-                        
                         if respuesta == "No tengo respuesta para esa pregunta, lo siento. Vamos a agregar la pregunta al sistema.":
                             print(f"{personaje}:", respuesta)
                             agregarPregunta()
@@ -404,6 +541,17 @@ def eleccionPersonaje(personaje):
                             print(f"{personaje}: Hazme otra pregunta")
                             return eleccionPersonaje(personaje)
                         print(f"{personaje}:", respuesta)
+                        if huboCorreccionOrtografica == True:
+                            correccion = input("Era la respuesta correcta? (si/no): ").lower().strip("¿?#$%&/()!¡-_[]}{.,;:<>")
+                            while correccion not in ["si", "no"]:
+                                correccion = input("No entendí, ¿era la respuesta correcta? (si/no): ").lower()
+                            if correccion == 'no':
+                                agregarPregunta()
+                                print(f"{personaje}: Hazme otra pregunta")
+                                return eleccionPersonaje(personaje)
+                            if correccion == 'si':
+                                print(f"{personaje}: Hazme otra pregunta")
+                        agregarInteraccionLogs(entradaOriginal, entradaModificada, respuesta, personaje)
                     except Exception as e:
                         manejarError(e, "Error procesando la pregunta")
     except Exception as e:
@@ -411,6 +559,17 @@ def eleccionPersonaje(personaje):
 
 
 def limpiadorFrases(input):
+    """
+    Limpia y tokeniza una cadena de entrada eliminando artículos y reemplazando vocales acentuadas.
+    Parámetros:
+        input (str): Cadena de entrada a limpiar y tokenizar.
+    Retorna:
+        list: Lista de palabras limpias de la cadena de entrada, excluyendo artículos y con vocales acentuadas reemplazadas.
+    Excepciones:
+        Si ocurre un error durante el procesamiento, se maneja con manejarError y retorna None.
+    Nota:
+        Esta función depende de las variables externas 'vocalesTildes', 'vocalesSinTilde', 'articulos' y la función 'manejarError'.
+    """
     palabraLimpia = []
     palabra = ''
 
@@ -442,6 +601,21 @@ def limpiadorFrases(input):
 
 
 def lectorPregunta(userInput, esYoda):
+    """
+    Lee preguntas y respuestas desde un archivo, procesa la entrada del usuario y retorna una respuesta apropiada.
+
+    Parámetros:
+        userInput (str): Entrada del usuario a comparar con las preguntas almacenadas.
+        esYoda (bool): Determina si se usan respuestas estilo Yoda ("YA:") o estándar ("A:").
+
+    Retorna:
+        str: La respuesta encontrada al comparar la entrada del usuario con las preguntas y respuestas almacenadas.
+
+    Excepciones:
+        KeyboardInterrupt: Si el usuario interrumpe la ejecución.
+        FileNotFoundError: Si el archivo o carpeta de preguntas no existe.
+        Exception: Para cualquier otro error durante la lectura del archivo.
+    """
     questGroup = []
     answGroup = []
     quest = []
@@ -487,6 +661,12 @@ def lectorPregunta(userInput, esYoda):
 
 # Manejo de errores
 def manejarError(e, mensaje):
+    """
+    Maneja errores capturados durante la ejecución del programa, reseteando la entrada original y registrando los detalles del error en un archivo de logs.
+    Args:
+        e (Exception): La excepción que fue capturada.
+        mensaje (str): Un mensaje descriptivo sobre el contexto o la causa del error.
+    """
     global entradaOriginal
     entradaOriginal = ''
     
@@ -496,8 +676,10 @@ def manejarError(e, mensaje):
             file.write(f"Error: {mensaje}\n")
             file.write(f"Detalles del error: {e}\n")
             file.write("-" * 50 + "\n")
+        print(f"Se ha producido un error: {mensaje}. Se ha registrado en el archivo de logs.")
     except Exception as e:
         print(f"Error al guardar el error en el archivo de registro: {e}")
+        
 
 
 # Ejecutar el programa principal
